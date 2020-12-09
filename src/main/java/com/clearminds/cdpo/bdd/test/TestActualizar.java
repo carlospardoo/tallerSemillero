92eef70a96151823d7e2ce44a051948f9ce20165
Carlos Pardo <28719833+carlospardoo@users.noreply.github.com>
2020-12-09 12:18:50 -0500

    Completo actualización de estudiantes

---
+++ src/main/java/com/clearminds/cdpo/bdd/test/TestActualizar.java
@@ -0,0 +1,20 @@
+package com.clearminds.cdpo.bdd.test;
+
+import com.clearminds.cdpo.dtos.Estudiante;
+import com.clearminds.cdpo.excepciones.BDDException;
+import com.clearminds.cdpo.servicios.ServicioEstudiante;
+
+public class TestActualizar {
+
+	public static void main(String[] args) {
+		ServicioEstudiante srvEstudiante = new ServicioEstudiante();
+		try {
+			srvEstudiante.actualizarEstudiante(new Estudiante("Pedro", "Navaja", 2));
+		} catch (BDDException e) {
+			e.printStackTrace();
+			e.getMessage();
+		}
+
+	}
+
+}
