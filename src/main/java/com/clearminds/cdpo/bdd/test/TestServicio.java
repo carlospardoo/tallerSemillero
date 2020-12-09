ccec9d31828aea9e00afa2c02ee9857442e39a9f
Carlos Pardo <28719833+carlospardoo@users.noreply.github.com>
2020-12-09 11:44:03 -0500

    Agregado metodo y test para ServicioEstudiante

---
+++ src/main/java/com/clearminds/cdpo/bdd/test/TestServicio.java
@@ -0,0 +1,19 @@
+package com.clearminds.cdpo.bdd.test;
+
+import com.clearminds.cdpo.dtos.Estudiante;
+import com.clearminds.cdpo.excepciones.BDDException;
+import com.clearminds.cdpo.servicios.ServicioEstudiante;
+
+public class TestServicio {
+
+	public static void main(String[] args) {
+		ServicioEstudiante srvEstudiante = new ServicioEstudiante();
+		try {
+			srvEstudiante.insertarEstudiante(new Estudiante("Juan", "Perez"));
+		} catch (BDDException e) {
+			e.printStackTrace();
+			e.getMessage();
+		}
+	}
+
+}
--- src/main/java/com/clearminds/cdpo/servicios/ServicioEstudiante.java
+++ src/main/java/com/clearminds/cdpo/servicios/ServicioEstudiante.java
@@ -1,5 +1,14 @@
 package com.clearminds.cdpo.servicios;
 
+import com.clearminds.cdpo.dtos.Estudiante;
+import com.clearminds.cdpo.excepciones.BDDException;
+
 public class ServicioEstudiante extends ServicioBase{
+	
+	public void insertarEstudiante(Estudiante estudiante) throws BDDException{
+		abrirConexion();
+		System.out.println("insertando estudiante:"+estudiante);
+		cerrarConexion();
+	}
 	
 }
