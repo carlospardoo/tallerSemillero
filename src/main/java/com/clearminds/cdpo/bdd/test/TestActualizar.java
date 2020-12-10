4c297a14134cc7c6973bbc3bc56bc5a3c9f1bda9
Carlos Pardo <28719833+carlospardoo@users.noreply.github.com>
2020-12-10 09:44:05 -0500

    Cambios ejercicio1

--- src/main/java/com/clearminds/cdpo/bdd/test/TestActualizar.java
+++ src/main/java/com/clearminds/cdpo/bdd/test/TestActualizar.java
@@ -9,7 +9,7 @@ public class TestActualizar {
 	public static void main(String[] args) {
 		ServicioEstudiante srvEstudiante = new ServicioEstudiante();
 		try {
-			srvEstudiante.actualizarEstudiante(new Estudiante("Pedro", "Navaja", 2));
+			srvEstudiante.actualizarEstudiante(new Estudiante("Pedro", "Navaja", 2,15));
 		} catch (BDDException e) {
 			e.printStackTrace();
 			e.getMessage();
